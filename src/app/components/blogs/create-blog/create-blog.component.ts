d91cb86f72232bf3dac501e76c7a57f0dbbb61a4
Ali Toghiani <ali.toghiani.dev@gmail.com>
2023-12-11 16:26:38 +0330

    feat(create-blog): add quill for blog content

--- src/app/components/blogs/create-blog/create-blog.component.ts
+++ src/app/components/blogs/create-blog/create-blog.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {QuillEditorComponent, QuillViewHTMLComponent} from "ngx-quill";
 
 @Component({
   selector: 'app-create-blog',
   standalone: true,
-  imports: [],
+  imports: [
+    QuillViewHTMLComponent,
+    QuillEditorComponent
+  ],
   templateUrl: './create-blog.component.html',
   styleUrl: './create-blog.component.scss'
 })
