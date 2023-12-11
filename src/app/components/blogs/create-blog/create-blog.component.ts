3f672c4fae29e15c736f94c6605cfe47caeb149d
Ali Toghiani <ali.toghiani.dev@gmail.com>
2023-12-11 21:18:06 +0330

    feat(create-blog): add the new blog to the blogs list

--- src/app/components/blogs/create-blog/create-blog.component.ts
+++ src/app/components/blogs/create-blog/create-blog.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import {QuillEditorComponent, QuillViewHTMLComponent} from "ngx-quill";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NgIf} from "@angular/common";
+import {BlogsModel} from "../../../models/blogs.model";
+import {MockDataService} from "../../../services/mock-data.service";
+import {Router} from "@angular/router";
+import {Paths} from "../../../enums/paths";
 
 @Component({
   selector: 'app-create-blog',
@@ -21,16 +25,36 @@ export class CreateBlogComponent {
   defaultImageURL = "https://images.unsplash.com/photo-1517048676732-d65bc937f952?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlJTIwYXQlMjB3b3JrfGVufDB8fDB8fHww";
   form = new FormGroup(
     {
-      title: new FormControl('', Validators.required),
-      image_url: new FormControl(this.defaultImageURL, Validators.required),
-      main_text: new FormControl('', Validators.required),
+      title: new FormControl('', { validators: Validators.required, nonNullable: true}),
+      image_url: new FormControl(this.defaultImageURL, { validators: Validators.required, nonNullable: true}),
+      main_text: new FormControl('', { validators: Validators.required, nonNullable: true}),
     }
   )
 
-  submit():void {
-    this.isLoading = true;
-    console.log( this.form.value)
+  constructor(
+    private mockDataService: MockDataService,
+    private router: Router
+  ) {
   }
 
+  submit():void {
+    this.isLoading = true;
 
+    const { title, image_url, main_text } = this.form.controls;
+
+    const data: BlogsModel = {
+      id: Math.floor(Math.random() * 9000) + 1000,
+      title: title.value,
+      image_url: image_url.value,
+      main_text: main_text.value,
+      date: (new Date()).toDateString(),
+      author_name: "John Doe",
+      author_avatar_url: 'https://randomuser.me/api/portraits/men/41.jpg',
+      comments: []
+    }
+    this.mockDataService.postBlog( data )
+    setTimeout( () => {
+      this.router.navigate(['/', Paths.BLOGS_LIST, data.id]);
+    }, 3000);
+  }
 }
