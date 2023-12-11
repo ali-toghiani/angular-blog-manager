8432b91760fa7f63e62f664d2574b63eab5a1eb2
Ali Toghiani <ali.toghiani.dev@gmail.com>
2023-12-11 21:20:35 +0330

    feat(app.config.ts): import HttpClientModue, BrowserAnimationsModule

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {QuillModule} from "ngx-quill";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule, provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(QuillModule.forRoot())
+    importProvidersFrom(QuillModule.forRoot()),
+    importProvidersFrom(HttpClientModule),
+    provideAnimations(),
+    importProvidersFrom(BrowserAnimationsModule),
   ]
 };
