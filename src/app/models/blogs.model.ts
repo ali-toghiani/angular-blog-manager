b4248ca2dab5fba9b56e56d613b4578eec05bdbb
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-09-02 16:36:34 +0330

    style(blogs.model.ts): format using prettier

--- src/app/models/blogs.model.ts
+++ src/app/models/blogs.model.ts
@@ -2,20 +2,20 @@ export interface BlogsListModel {
   data: BlogsModel[];
 }
 
-export interface BlogsModel{
-  id: number
-  title: string
-  image_url: string
-  main_text: string
-  date: string
-  author_name: string
-  author_avatar_url: string
-  comments: BlogCommentModel[]
+export interface BlogsModel {
+  id: number;
+  title: string;
+  image_url: string;
+  main_text: string;
+  date: string;
+  author_name: string;
+  author_avatar_url: string;
+  comments: BlogCommentModel[];
 }
 
 export interface BlogCommentModel {
-  name: string
-  message: string
-  date: string
-  avatar_url: string
+  name: string;
+  message: string;
+  date: string;
+  avatar_url: string;
 }
